03b0e6bf70d43a3f601d7fb9d14279f70f45b6b2
Pedro Abreu <pedro.schmid2311@gmail.com>
2024-10-02 17:51:05 -0300

    chore: add keys with env vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,6 +8,16 @@ variable "aws_region" {
   description = "AWS region where the resources will be deployed (e.g., us-east-1)."
 }
 
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+}
+
 variable "vpc_cidr" {
   type        = string
   description = "CIDR block for the VPC to define the range of IP addresses."
